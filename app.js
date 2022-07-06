87d9be546698e8fe441a380c42363ebb1645603b
Rashed Amer <rashedmalkawi97@gmail.com>
2022-07-06 22:09:05 +0300

    contries flags

---
+++ app.js
@@ -0,0 +1,43 @@
+let myRequest=new XMLHttpRequest();
+myRequest.open("GET","https://restcountries.com/v2/all");
+myRequest.send();
+
+myRequest.onreadystatechange=function(){
+    if(this.readyState===4 && this.status===200){
+
+        let jsData=JSON.parse(this.responseText);
+            console.log(jsData);
+
+
+        for(let i=0; i<jsData.length; i++){
+
+            let div=document.createElement("div");
+            let contName=document.createTextNode(jsData[i].name);
+            let contFlag=jsData[i].flag;
+            let h1=document.createElement("h1");
+            h1.appendChild(contName);
+            let img=document.createElement("img");
+            img.src=contFlag;
+          
+            div.appendChild(h1);
+            div.appendChild(img);
+
+            for( j=0; j<jsData[i].languages.length ; j++ ){
+                let contLang=jsData[i].languages[j].name;
+                let span=document.createElement("span");
+                span.textContent=contLang;
+                div.appendChild(span);
+            }
+            
+
+            let container=document.getElementById('container');   
+            container.appendChild(div);
+            document.body.appendChild(container);
+            
+
+
+
+        }
+    }
+    
+}
